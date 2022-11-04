1121715e6c8a00555f7808d33861e3bbd83408bc
ylan <hei.nalisoa@gmail.com>
2022-11-04 15:21:11 +0300

    fix : bad import

--- src/main/java/school/hei/haapi/service/PlaceService.java
+++ src/main/java/school/hei/haapi/service/PlaceService.java
@@ -2,11 +2,8 @@ package school.hei.haapi.service;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import school.hei.haapi.model.Course;
 import school.hei.haapi.model.Place;
-import school.hei.haapi.repository.CourseRepository;
 import school.hei.haapi.repository.PlaceRepository;
 
 import java.util.List;
@@ -15,18 +12,18 @@ import java.util.List;
 @AllArgsConstructor
 
 public class PlaceService {
-    private PlaceRepository placeRepository ;
+  private PlaceRepository placeRepository ;
 
-    public List<Place> getPlaces(){
-        return placeRepository.findAll() ;
-    }
+  public List<Place> getPlaces(){
+    return placeRepository.findAll() ;
+  }
 
-    public Place createOrUpdatePlaces(Place place){
-        return placeRepository.save(place) ;
-    }
+  public Place createOrUpdatePlaces(Place place){
+    return placeRepository.save(place) ;
+  }
 
-    public Place getPlacesById(String id){
-        return placeRepository.findById(id).get() ;
-    }
+  public Place getPlacesById(String id){
+    return placeRepository.findById(id).get() ;
+  }
 
 }
